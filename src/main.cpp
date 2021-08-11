9bd4367a809c3c32350221dd9c88f5d0fb78e4db
MaciekN <me@maciekn.pl>
2021-08-11 22:01:35 +0200

    Add sending to mqqt

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,79 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
 #include "izar_wmbus.h"
 
 extern uint32_t meterId;
+extern const char* ssid;
+extern const char* password;
+extern const char* mqttServer;
+extern const int mqttPort;
 
 IzarWmbus reader;
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        // Attempt to connect
+        if (client.connect(clientId.c_str())) {
+            Serial.println("connected");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
 
 void setup() {
     Serial.begin(9600);
     reader.init(meterId);
-}
 
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    client.setServer(mqttServer, mqttPort);
+}
 
 IzarResultData data;
 
 void loop() {
-    if(reader.fetchPacket(&data) == FETCH_SUCCESSFUL) {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+
+
+    if (reader.fetchPacket(&data) == FETCH_SUCCESSFUL) {
+        StaticJsonDocument<200> doc;
+        doc["meter"] = data.meterId;
+        doc["usg"] = data.waterUsage;
+        char buffer[256];
+        size_t n = serializeJson(doc, buffer);
+        client.publish("water/consumption", buffer, n);
+
+
         Serial.print("WatermeterId: ");
         Serial.println(data.meterId, HEX);
 
@@ -24,6 +82,4 @@ void loop() {
     } else {
         delay(300);
     }
-    
-
 }
